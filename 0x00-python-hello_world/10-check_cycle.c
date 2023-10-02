80bb2f6958b1cf69fbeb8d58bc1d6ac9f0f5d858
AMR856 <amer.live477@gmail.com>
2023-10-02 18:25:11 +0300

    Try This

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - A function to check if there is a cycle or not
+ * @list: A pointer to the head of the list
+ * 
+ * Return: 1 if there is a cycle and 0 if not
+*/
+
+int check_cycle(listint_t *list)
+{
+    listint_t *myFirstHead = list;
+    listint_t *mySecondHead = list;
+    while (myFirstHead != NULL && mySecondHead != NULL)
+    {
+        myFirstHead = myFirstHead->next;
+        mySecondHead = mySecondHead->next->next;
+        if (myFirstHead == mySecondHead)
+            return (1);
+    }
+    return (0);
+}
