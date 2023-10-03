e1062208c74baab58269a687e60a559f4c47e380
AMR856 <amer.live477@gmail.com>
2023-10-03 15:25:39 +0300

    Fourth Task and fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-import string
-x = string.ascii_lowercase
-print(x, end='')
-
-# for i in range(97, 123):
-#     print(chr(i),end='')
+for i in range(97, 123):
+    print(chr(i),end='')
