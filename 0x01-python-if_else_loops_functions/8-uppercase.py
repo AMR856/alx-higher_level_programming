6a3637c43ba7ef7e0c0603eab77cd84ed2a8ce25
AMR856 <amer.live477@gmail.com>
2023-10-03 16:11:56 +0300

    Try

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    h = 0
+    k = len(str)
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 123:
+            v = ord(i)
+            print("{}".format(chr((v - 32))), end=""if h != k - 1 else '\n')
+            h = h + 1
+            continue
+        print("{}".format(i), end="" if h != k - 1 else '\n')
+        h = h + 1
