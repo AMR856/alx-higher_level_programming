26c225ff4838cc0ad072a09a4492bd74b1c78e01
AMR856 <amer.live477@gmail.com>
2023-10-03 15:55:18 +0300

    fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     """A function to check if a char is lower or upper case"""
-    if c >= 'a' and c <= 'z':
+    if ord(c) >= 97 and ord(c) <= 123:
         return True
     else:
         return False
