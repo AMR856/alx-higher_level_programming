858fd78fe16372a96bced22b293637d83b039788
AMR856 <amer.live477@gmail.com>
2023-10-03 18:17:39 +0300

    Try this

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * insert_node - A function to insert a node in a sorted
+ * linked list
+ * @head: A Pointer to a pointer that points
+ * to the head of the linked list
+ * @number: The number that will be put in the new node
+ *
+ * Return: NULL or a pointer to the new node
+*/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tempHead = *head;
+	listint_t *newNode = (listint_t *)malloc(sizeof(listint_t));
+
+	newNode->n = number;
+	while (tempHead != NULL)
+	{
+		if (number > tempHead->n && number < tempHead->next->n)
+		{
+			newNode->next = tempHead->next;
+			tempHead->next = newNode;
+			return (newNode);
+		}
+		tempHead = tempHead->next;
+	}
+	return (NULL);
+}
