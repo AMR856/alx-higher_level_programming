7c22ab148a02e6b2f97cbfa174e0c7c54cdbebe8
AMR856 <amer.live477@gmail.com>
2023-10-03 15:52:41 +0300

    Eigth Task

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    """A function to check if a char is lower or upper case"""
+    if c >= 'a' and c <= 'z':
+        return True
+    else:
+        return False
