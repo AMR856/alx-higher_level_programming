7e6b4b92c9374ccb7282359481c33eb7eb8cce66
AMR856 <amer.live477@gmail.com>
2023-10-07 19:58:22 +0300

    dk

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,26 +11,53 @@
  * Return: 1 if palindrome and 0 if not
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - A function to check if a linked list is palindrome
+ * or not
+ * @head: A pointer to a pointer that points to the head
+ *
+ * Return: 1 if palindrome and 0 if not
+*/
+
 int is_palindrome(listint_t **head)
 {
-	int myArray[MAX_SIZE] = {0};
-	listint_t *tempHead = *head;
-	int counter = 0, anLooper, flag = 1;
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *next;
+    int isPalindrome = 1;
 
-	while (tempHead != NULL)
-	{
-		myArray[counter] = tempHead->n;
-		counter++;
-		tempHead = tempHead->next;
-	}
-	anLooper = counter - 1;
-	for (int i = 0; i < counter / 2; i++, anLooper--)
-	{
-		if (myArray[i] != myArray[anLooper])
-		{
-			flag = 0;
-			break;
-		}
-	}
-	return (flag);
+    if (*head == NULL || (*head)->next == NULL)
+        return (isPalindrome);
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        next = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = next;
+    }
+
+    if (fast != NULL)
+    {
+        slow = slow->next;
+    }
+
+    while (prev != NULL)
+    {
+        if (prev->n != slow->n)
+        {
+            isPalindrome = 0;
+            break;
+        }
+        prev = prev->next;
+        slow = slow->next;
+    }
+
+    return (isPalindrome);
 }
