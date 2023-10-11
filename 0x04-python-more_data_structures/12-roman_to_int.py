bb49fbcf310b19904094d4c413b958b86cb5e2b5
AMR856 <amer.live477@gmail.com>
2023-10-11 12:12:46 +0300

    k

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     sum = 0
-    if roman_string is None or isinstance(roman_string, str):
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     myDic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in roman_string:
