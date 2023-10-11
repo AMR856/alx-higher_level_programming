e60222f99f32e33a95e154b6ac049e8a1f5d6d36
AMR856 <amer.live477@gmail.com>
2023-10-11 12:14:08 +0300

    annoying checker

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
     newList = my_list.copy()
     for i in range(0, len(newList) - 1):
         if newList[i] == search:
