86f4aaa9045b73579168d4c1bdd225a676634e0a
AMR856 <amer.live477@gmail.com>
2023-10-23 14:40:14 +0300

    Checker is killing

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,8 +2,7 @@
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except Exception as Anything:
         sys.stderr.write("Exception: {}\n".format(Anything))
         return False
-
+    return True
