42d6b9765ac821bc1c5b012dc13a762834012a4c
AMR856 <amer.live477@gmail.com>
2023-10-23 14:38:18 +0300

    Stuff

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as Anything:
+        sys.stderr.write("Exception: {}\n".format(Anything))
         return False
 
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     myNewList = []
-    addedNum = 0
-    for i in range(list_length):
-        try:
-            result = my_list_1[i] / my_list_2[i]
-            addedNum = result
-        except TypeError:
-            print("wrong type")
-            addedNum = 0
-        except ZeroDivisionError:
-            print("division by zero")
-            addedNum = 0
-        except IndexError:
-            print("out of range")
-            addedNum = 0
-        finally:
-            myNewList.append(addedNum)
-
+    try:
+        for i in range(list_length):
+            try:
+                result = my_list_1[i] / my_list_2[i]
+                myNewList.append(result)
+            except TypeError:
+                print("wrong type")
+                myNewList.append(0)
+            except ZeroDivisionError:
+                print("division by zero")
+                myNewList.append(0)
+    except IndexError:
+        print("out of range")
+        while i < list_length:
+            myNewList.append(0)
+            i = i + 1
+    finally:
+        return myNewList
