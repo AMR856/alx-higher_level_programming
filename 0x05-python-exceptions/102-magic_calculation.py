42b7d6570b291ba039ed69a497f0f6fc933b2077
AMR856 <amer.live477@gmail.com>
2023-10-23 16:57:43 +0300

    Weird

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,8 +5,8 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception("too far")
-            result += (a ** b) / i
+            result += a ** b / i
         except Exception:
-            result = b + a
+            result = a + b
             break
         return result
