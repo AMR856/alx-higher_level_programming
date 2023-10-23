cae2705dd55fafd0ddf30170f54fac627e38e969
AMR856 <amer.live477@gmail.com>
2023-10-23 14:29:30 +0300

    Weird checker

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     myNewList = []
+    addedNum = 0
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
-            myNewList.append(result)
+            addedNum = result
         except TypeError:
             print("wrong type")
-            myNewList.append(0)
+            addedNum = 0
         except ZeroDivisionError:
             print("division by zero")
-            myNewList.append(0)
+            addedNum = 0
         except IndexError:
             print("out of range")
-            myNewList.append(0)
-    return myNewList
+            addedNum = 0
+        finally:
+            myNewList.append(addedNum)
+
