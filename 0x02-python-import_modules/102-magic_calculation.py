2e2d284ad3e25ea7dbada11b5337bd5481255c5e
AMR856 <amer.live477@gmail.com>
2023-10-23 15:22:46 +0300

    now

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # print_list_integer = __import__
 def magic_calculation(a, b):
-    add, sub = __import__(('add', 'sub')).magic_calculation_102
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
