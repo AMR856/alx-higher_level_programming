51f7735a0f49559eb129e7f4cb6c2a1d1a789cfa
AMR856 <amer.live477@gmail.com>
2023-10-23 12:50:15 +0300

    Dk

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-            except TypeError:
-                break
-        print()
-        return i + 1
-    except IndexError:
-        print()
-        return i
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except Exception as Anything:
+            break
+    print()
+    return i + 1
