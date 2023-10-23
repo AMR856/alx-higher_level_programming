e40d58f1e744c830c37503218f8cd26181f4b621
AMR856 <amer.live477@gmail.com>
2023-10-23 12:50:39 +0300

    Look

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,5 +5,5 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
         except Exception as Anything:
             break
-    print()
+    print('')
     return i + 1
