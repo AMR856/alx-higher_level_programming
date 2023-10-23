0c00c3957bc8942c826bc0d70e02305ea196c093
AMR856 <amer.live477@gmail.com>
2023-10-23 12:51:15 +0300

    cameback

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-        except Exception as Anything:
-            break
-    print('')
-    return i + 1
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+            except TypeError:
+                break
+        print()
+        return i + 1
+    except IndexError:
+        print()
+        return i
