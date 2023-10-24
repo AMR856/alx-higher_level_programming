4ad7e7f45f4f04214ad18f9ca3f7d540db1af552
AMR856 <amer.live477@gmail.com>
2023-10-24 15:31:53 +0300

    just commit dc now

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Making a new class called Square"""
+
+
+class Square:
+    """My square class"""
+    def __init__(self, size=0):
+        """Making the size attibute private and assigning its value
+
+        Args:
+            It's the size of the created object
+
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """A getter for the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A setter for the size
+
+        Args:
+            value of the size of the to be set
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Getting the area of the square
+
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """It's a method to print some hashtags on the screen
+
+        """
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print()
