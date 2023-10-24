8cfd046a982e266692311d60669fa1d00a1b44cf
AMR856 <amer.live477@gmail.com>
2023-10-24 23:17:38 +0300

    Try

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,29 +6,29 @@ class Node:
     """My Node"""
     def __init__(self, data, next_node=None):
         """The init for my Node
-        
+
         Args:
             data: it's the data to be put in the list
             next_node: The next node
         """
         self.data = data
         self.next_node = next_node
-    
+
     @property
     def data(self):
         """The getter for my data"""
         return self.__data
-    
+
     @data.setter
     def data(self, value):
         """The setter for my data
-        
+
         Args: The value of the data to be put
         """
-        if not isinstance(size, int):
+        if not isinstance(value, int) and value is not None:
             raise TypeError("data must be an integer")
         self.__data = value
-    
+
     @property
     def next_node(self):
         """The getter for the next_node"""
@@ -37,20 +37,48 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         """The setter for my next_node
-        
+
         Args: The next_node to be put in the list
         """
-        if value == None or isinstance(value, Node):
+        if value is None or isinstance(value, Node):
             self.__next_node = value
         else:
             raise TypeError("next_node must be a Node object")
 
+
+"""Defining My linked list"""
+
+
 class SinglyLinkedList:
     def __init__(self):
-        self.__head = head
-    
+        """The init for my list"""
+        self.head = Node(0, None)
+
     def __str__(self):
+        """This __str__ method that will let me print the
+        linked list using print
+        """
+        current = self.head.next_node
+        linked_list_str = ""
+        while current is not None:
+            linked_list_str += str(current.data) + "\n"
+            current = current.next_node
+        return linked_list_str.strip()
+
+    def sorted_insert(self, value):
+        """A method to add a node in a sorted linked list
+
+        Args: The value of the added node
+
+        """
+        newNode = Node(value)
         current = self.head
-        while (current != None):
-            print(current.data)
-            current = current.next
+        if current.next_node is None or newNode.data < current.data:
+            newNode.next_node = current.next_node
+            current.next_node = newNode
+        else:
+            while current.next_node is not None and \
+                    newNode.data > current.next_node.data:
+                current = current.next_node
+            newNode.next_node = current.next_node
+            current.next_node = newNode
