2c7faba526728d9102fb5b669e22dad4f5185355
Amr_Alnas <46923038+AMR856@users.noreply.github.com>
2023-10-24 18:58:40 +0300

    Go

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     theSub = 0
+    count = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
+            count = count + 1
         except Exception as Anything:
             theSub = theSub + 1
             pass
     print()
-    return i + 1 - theSub
+    return count - theSub
