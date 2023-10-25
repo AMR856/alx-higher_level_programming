4603c321b68b246b0b4d141f8632117528fa2ee3
AMR856 <amer.live477@gmail.com>
2023-10-25 17:22:47 +0300

    go

--- 0x06-python-classes/6-main.py
+++ 0x06-python-classes/6-main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 Square = __import__('6-square').Square
-my_square_2 = Square(3, (1, 1))
+my_square_2 = Square(0, (10, 3))
 my_square_2.my_print()
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,6 +65,7 @@ class Square:
         """
         if self.__size == 0:
             print()
+            return
         for i in range(self.__position[1]):
             print()
         for i in range(self.__size):
