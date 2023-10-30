cf04ad66ec118297626ba2410c1389faa3ab48fd
AMR856 <amer.live477@gmail.com>
2023-10-30 23:15:59 +0200

    Commit now

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""This is my class of the rectangle"""
+
+
+class Rectangle:
+    """Here is the class"""
+    def __init__(self, width=0, height=0):
+        """My init function of the rectangle
+
+        Args:
+            width: It's the width of my rectangle
+            height: It's the height of my rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """The getter for my width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """The setter for my width
+
+        Args: The value of the width
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """The getter for my height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """The setter for my height
+
+        Args: The value of the height
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """A method to get my area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """A method to get the perimeter"""
+        if (self.__height == 0 or self.__width == 0):
+            return 0
+        return (2 * (self.__height + self.__width))
