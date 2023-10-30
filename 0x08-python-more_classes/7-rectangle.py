a04d0e87722370a14edccb91642878f47100ceee
AMR856 <amer.live477@gmail.com>
2023-10-31 00:17:45 +0200

    ok this time leave me alone for real

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """Here is the class"""
     number_of_instances = 0
     print_symbol = '#'
-    
+
     def __init__(self, width=0, height=0):
         """My init function of the rectangle
 
