5c2df527ca3fc99e1591307aaa203897e0f66a5b
AMR856 <amer.live477@gmail.com>
2023-10-31 18:23:30 +0200

    just go

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,9 @@
 """Creating a locked class here"""
 
 
-def LockedClass():
+class LockedClass:
     """Here is my class and nothing speical about it"""
-    __slots__ = ["first_name"]
+    __slots__ = ("first_name", )
+
+    def __init__(self, first_name):
+        self.first_name = first_name
