6482941d75331db23b6e137dabd6d9cbf1c586ab
AMR856 <amer.live477@gmail.com>
2023-11-03 14:14:41 +0200

    This better

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+def matrix_mul(m_a, m_b):
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
+
+    if len()
