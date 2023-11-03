f4bc81c49c7df8f2a280f21ae8b78375ae0711bc
AMR856 <amer.live477@gmail.com>
2023-11-03 14:42:44 +0200

    go idiot

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module to mastu*** in NNN in matrices"""
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """A function to get you what you want
+    
+    Args: First thing
+    second Thing
+"""
+    return (np.dot(m_a, m_b))
---
+++ 0x07-python-test_driven_development/myPy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import numpy as np
+
+# Define two matrices
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[5, 6], [7, 8]])
+
+# Perform matrix multiplication
+result = matrix1 @ matrix2
+
+# Print the result
+print(result)
