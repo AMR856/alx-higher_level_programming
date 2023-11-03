475fc1b3a9654436dc38adbd5f0a6971ab61f178
AMR856 <amer.live477@gmail.com>
2023-11-03 14:20:49 +0200

    might work

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A function to print your first and last name"""
+
+
 def say_my_name(first_name, last_name=""):
     """"Here is the function
 
@@ -11,5 +13,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("My name is ", end= "")
+    print("My name is ", end="")
     print(first_name + " " + last_name)
