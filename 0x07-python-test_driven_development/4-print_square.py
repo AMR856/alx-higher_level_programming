7520ac38bc4a0a3a6ae2badf3f76a8eb8eca7979
AMR856 <amer.live477@gmail.com>
2023-11-03 14:22:22 +0200

    Here

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """A function to print a square of hashtags"""
+
+
 def print_square(size):
     """Here is my function
-    
+
     Args: size is the number of row and columns
     """
     if type(size) is not int and type(size) is not float:
@@ -10,13 +12,13 @@ def print_square(size):
 
     if size < 0 and type(size) is int:
         raise TypeError("size must be >= 0")
-    
+
     if size < 0 and type(size) is float:
         raise TypeError("size must be an integer")
 
     if size == 0:
-        print()
-    
+        return
+
     for i in range(int(size)):
         for j in range(int(size)):
             print("#", end="")
