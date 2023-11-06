7d36a125360112b1f371ebbb2381e5267e12125b
ameralnas <amer.live477@gmail.com>
2023-11-06 11:39:35 +0200

    Go for now

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Just a weird function to check something"""
+
+
+def is_same_class(obj, a_class):
+    """A function to check if an instance or not
+
+    Args: The object to be checked
+    The class or the target we can say
+    """
+    return (type(obj) is a_class)
