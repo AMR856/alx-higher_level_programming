b47d9a8ed1b71456782e79aef439a22318f0072a
ameralnas <amer.live477@gmail.com>
2023-11-06 14:50:39 +0200

    Try this

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Making my base geometry class"""
+
+
+class BaseGeometry:
+    """Here is the class"""
+    def area(self):
+        """A method to get the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A method to the value
+        Args: The name of the ...
+        The value of the ...
+        """
+
+        if (type(value) is int):
+            raise TypeError("{} must be an integer".format(name))
+        if (value <= 0):
+            raise ValueError("{} must be greater than 0".format(name))
