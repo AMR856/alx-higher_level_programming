81b42b8bac7f01784bd0f9eb80686cf4c02b0677
ameralnas <amer.live477@gmail.com>
2023-11-06 17:11:39 +0200

    Go now bro plzzzz.

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""A function to make you think about your life"""
+
+
 def add_attribute(myObj, TheAtt, TheValue):
+    """A weird function but handy sometimes
+
+    Args:
+        The attribute you want to set
+        The value of the attribute
+    """
     if hasattr(myObj, '__dict__') or \
             (hasattr(myObj, '__slots__') and attr_name in myObj.__slots__):
         setattr(myObj, TheAtt, TheValue)
