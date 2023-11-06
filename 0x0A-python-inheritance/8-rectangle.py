db4ad3fcc0496a37d38ea68273575137d3019713
ameralnas <amer.live477@gmail.com>
2023-11-06 15:37:40 +0200

    let's see this one

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Here we make a new module called rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Here is rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """The init of my clas
+
+        Args:
+            width:The width of the rec
+            height:The height of the rec
+        """
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self__width = width
+        self__height = height
