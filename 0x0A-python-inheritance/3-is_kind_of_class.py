7dfd969abd5a07e463698728f9646472db64eb4e
ameralnas <amer.live477@gmail.com>
2023-11-06 11:46:44 +0200

    I think that will work

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A function to check typessness"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Here is the function
+
+    Args: The object
+    The class"""
+    return (isinstance(obj, a_class))
