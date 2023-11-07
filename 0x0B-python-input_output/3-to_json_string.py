2f424a8deb45b9edb23f5ed7b26452ace6684d6c
ameralnas <amer.live477@gmail.com>
2023-11-07 13:23:30 +0200

    A start

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""A function to convert to json"""
+
+
+def to_json_string(my_obj):
+    """Here is the function
+
+    Args: The object"""
+    return (json.dumps(my_obj))
