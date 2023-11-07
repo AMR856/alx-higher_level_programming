911a57310b650c58e22dc3bcc79a3ca332c9bd4a
ameralnas <amer.live477@gmail.com>
2023-11-07 11:51:36 +0200

    Wrong number

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A program to write to a file"""
+
+def write_file(filename="", text=""):
+    """My function
+
+    Args: The file we want to write to
+    the Text to be written
+    """
+    with open(filename, 'w', encoding='utf-8') as myFile:
+        return (myFile.write(text))
