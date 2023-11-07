439a6183bda72e16903fd0c8be6aa894bd18dde6
ameralnas <amer.live477@gmail.com>
2023-11-07 16:42:11 +0200

    Maybe this one

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,26 +3,33 @@
 import sys
 
 
+def printingMyInfo():
+    print("File size: {:d}".format(myFinalSize))
+    for i, j in sorted(myDic.items()):
+        if j > 0:
+            print("{:}: {:d}".format(i, j))
+
+
 counter = 0
-myDic = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+myDic = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 myFinalSize = 0
 try:
-    for line in sys.stdin:
-        myList = line.split()
-        myCode = int(myList[7])
-        mySize = int(myList[8])
-        myFinalSize = myFinalSize + mySize
-        myDic[myCode] = myDic[myCode] + 1
+    for myLine in sys.stdin:
+        if counter != 0 and counter % 10 == 0:
+            printingMyInfo()
+        tryingToget = myLine.split()
+        try:
+            myStatus = tryingToget[-2]
+            if myStatus in myDic.keys():
+                myDic[myStatus] = myDic[myStatus] + 1
+        except Exception as Everything:
+            pass
+        try:
+            myFinalSize = int(tryingToget[-1]) + myFinalSize
+        except Exception as Everything:
+            pass
         counter = counter + 1
-        if counter == 10:
-            print("File size: {:d}".format(myFinalSize))
-            for i, j in sorted(myDic.items()):
-                if j > 0:
-                    print("{:}: {:d}".format(i, j))
-            counter = 0
+    printingMyInfo()
 except KeyboardInterrupt:
-    print("File size: {:d}".format(myFinalSize))
-    for i, j in myDic.items():
-        if j < 0:
-            print("{:}: {:d}".format(i, j))
+    printingMyInfo()
     raise
