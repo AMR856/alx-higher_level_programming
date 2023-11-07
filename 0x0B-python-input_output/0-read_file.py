64a7a48fabadd23acb2f0af6a04f7c327875d36f
ameralnas <amer.live477@gmail.com>
2023-11-07 11:35:37 +0200

    just

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,10 +4,10 @@
 
 def read_file(filename=""):
     """Here is the function for reading
-        
+
     Args: The file name
     """
-    if (len(filename) == 0):
+    if (len(filename) == 0 or filename is None):
         return
     with open(filename, 'r', encoding='utf-8') as myFile:
         for line in myFile:
