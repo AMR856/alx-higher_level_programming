c87c153d5b7217c92e63887a792eaba05a6d4b52
ameralnas <amer.live477@gmail.com>
2023-11-09 20:33:19 +0200

    Let's add this

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""Here is my rectangle module"""
+from base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Here is init function
+
+        Args:
+            width: The width of my rec
+            height: The height of my rec
+            x: The x value of my rec
+            y: The y value of my rec
+        """
+        self.width = width
+        self. height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    """The getter and setter for my width"""
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, myValue):
+        if type(myValue) is not int:
+            raise TypeError("{} must be an integer".format("width"))
+        if myValue <= 0:
+            raise ValueError("{} must be > 0".format("width"))
+        self.__width = myValue
+
+    """The getter and setter for my height"""
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, myValue):
+        if type(myValue) is not int:
+            raise TypeError("{} must be an integer".format("height"))
+        if myValue <= 0:
+            raise ValueError("{} must be > 0".format("height"))
+        self.__height = myValue
+
+    """The getter and setter for my x"""
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, myValue):
+        if type(myValue) is not int:
+            raise TypeError("{} must be an integer".format("x"))
+        if myValue < 0:
+            raise ValueError("{} must be > 0".format("x"))
+        self.__x = myValue
+
+    """The getter and setter for my y"""
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, myValue):
+        if type(myValue) is not int:
+            raise TypeError("{} must be an integer".format("y"))
+        if myValue < 0:
+            raise ValueError("{} must be >= 0".format("y"))
+        self.__y = myValue
+
+    def area(self):
+        """A method to get the area"""
+        return self.__width * self.__height
+
+    def display(self):
+        """A method to print # on the screen"""
+        for i in range(self.__y):
+            print()
+        for i in range(self.__height):
+            print(" " * self.__x, end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """My str method for printing"""
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                self.__x, self.__y, self.__width, self.__height))
