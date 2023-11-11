6bae9e8e9d8dfd62d96712d2a9b066703fbc9029
ameralnas <amer.live477@gmail.com>
2023-11-11 11:59:07 +0200

    Go for now

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -70,3 +70,37 @@ class Base:
 
     def load_from_file_csv(cls):
         pass
+
+    @classmethod
+    def create(cls, **dictionary):
+        """A method to create the object
+
+        Args: The kwargs for my update function
+        """
+        if cls.__name__ == "Rectangle":
+            myDummyR = cls(1, 1, 0, 0, None)
+            cls.update(dictionary)
+            return myDummyR
+        elif cls.__name__ == "Square":
+            myDummyS = cls(1, 0, 0, None)
+            cls.update(dictionary)
+            return myDummyS
+
+    @classmethod
+    def load_from_file(cls):
+        """A method to load an object from json file
+        
+        Args: Nothing
+        """
+        try:
+            with open(f"{cls.__name__}.json", "r", encoding="utf-8") as myFile:
+                myList = []
+                theJsonString = myFile.read()
+                mylistAfterJson = Base.from_json_string(theJsonString)
+                for i in mylistAfterJson:
+                    myList.append(cls.create(**i))
+                return myList
+        except FileNotFoundError:
+            return []
+        
+    # [{'id': 1, 'width': 10, 'height': 7, 'x': 2, 'y': 8}, {'id': 2, 'width': 2, 'height': 4, 'x': 0, 'y': 0}]
