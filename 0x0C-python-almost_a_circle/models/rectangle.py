31f5f95d161e32e667b35a85585f9419fc0cd35c
ameralnas <amer.live477@gmail.com>
2023-11-14 11:43:35 +0200

    Tripping

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Here is my rectangle module"""
-from base import Base
+from models.base import Base
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Here is my square class"""
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
