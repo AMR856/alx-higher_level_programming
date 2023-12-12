10fdf274eb6fbd8bb8092e45bd35c716fa1b8436
ameralnas <amer.live477@gmail.com>
2023-12-12 22:34:03 +0200

    Finishedall

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- I'm converting to some decoding things
-ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,4 @@
+-- Getting the max
+SELECT state, MAX(value) AS max_temp FROM temperatures
+GROUP BY state
+ORDER BY state;
