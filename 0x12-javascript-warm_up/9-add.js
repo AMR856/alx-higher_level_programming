3a713aa77b06ccff064ad5a73ea1f38d69330824
ameralnas <amer.live477@gmail.com>
2024-01-25 03:41:39 +0200

    Weird

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 function add (a, b) {
-  const c = Number(a) + Number(b);
-  console.log(c);
-}
-const process = require('process');
-if (process.argv.length === 2 || process.argv.length === 3) {
-  console.log(NaN);
-} else {
-  add(process.argv[2], process.argv[3]);
-}
+    const c = a + b;
+    console.log(c);
+  }
+
+add(Number(process.argv[2]), Number(process.argv[3]));
