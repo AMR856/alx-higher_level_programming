a2e3fdb8804f89f85523e6623798b6e086b37114
ameralnas <amer.live477@gmail.com>
2024-01-25 19:18:35 +0200

    Go

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
   constructor (w, h) {
-    this.height = h;
-    this.width = w;
+    if (h > 0 && w > 0) {
+      this.height = h;
+      this.width = w;
+    }
   }
 }
-
 module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
