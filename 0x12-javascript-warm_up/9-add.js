9dc33fc70589b26177ca98c6ea281ef78ebe956f
ameralnas <amer.live477@gmail.com>
2024-01-25 03:39:54 +0200

    Hi

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 function add (a, b) {
-  console.log(String(Number(a) + Number(b)));
+  const c = Number(a) + Number(b);
+  console.log(c);
 }
 const process = require('process');
 if (process.argv.length === 2 || process.argv.length === 3) {
-  console.log('NaN');
+  console.log(NaN);
 } else {
   add(process.argv[2], process.argv[3]);
 }
