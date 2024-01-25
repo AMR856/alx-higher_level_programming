300ac3a838960a93ab52f4a6e415a44fb8a1edce
ameralnas <amer.live477@gmail.com>
2024-01-25 03:42:45 +0200

    Found it

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 function add (a, b) {
-    const c = a + b;
-    console.log(c);
-  }
-
-add(Number(process.argv[2]), Number(process.argv[3]));
+  console.log(Number(a) + Number(b));
+}
+const process = require('process');
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(NaN);
+} else {
+  add(process.argv[2], process.argv[3]);
+}
