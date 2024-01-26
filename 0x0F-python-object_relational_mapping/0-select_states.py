2b7d9e4c2ee1fb811ce24043464ddc660b78dbdd
ameralnas <amer.live477@gmail.com>
2024-01-26 16:05:00 +0200

    Starting Python and database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user = sys.argv[1], passwd = sys.argv[2],
+                        db = sys.argv[3], port=3306)
+    myCursor = db.cursor()
+    myCursor.execute("SELECT * FROM states")
+    myRows = myCursor.fetchall()
+    for row in myRows:
+        print(row)
+    myCursor.close()
+    db.close()
