231c6488abfc6abf2e52b5bab5a11b7fbc148109
ameralnas <amer.live477@gmail.com>
2024-01-27 03:27:20 +0200

    Fixing style

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
-from model_state import Base, State
 """Here I write some classes"""
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
 class City(Base):
+    "City class here"
     __tablename__ = 'cities'
     id = Column(Integer, nullable=False, primary_key=True, unique=True)
     name = Column(String(128), nullable=False)
