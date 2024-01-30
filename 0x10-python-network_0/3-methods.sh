2ecb1e44d1c6a4b76e2d8fce5da4189fa1b57649
ameralnas <amer.live477@gmail.com>
2024-01-30 23:21:11 +0200

    Hi again

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script to see options
-curl -sI -X OPTIONS "$1" | sed -n '6p' | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
+curl -sI "$1" | grep "Allow" | cut -d ' ' -f 2-
