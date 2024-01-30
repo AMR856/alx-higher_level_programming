85cba4fe824a2daac6fae3f5f64085578184304b
ameralnas <amer.live477@gmail.com>
2024-01-30 22:33:55 +0200

    again

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # A script to curlllllll
-http_status=$(curl -s -L  -o /dev/null -w "%{http_code}" "$1")
-response_body=$(curl -s -L "$1")
-if [ $http_status -eq 200 ]; then
-    echo -n "$response_body"
-fi
+curl -L "$1"
