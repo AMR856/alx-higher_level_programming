fd9dfb4cc18050e3c2269baec673c71b8745a8b7
ameralnas <amer.live477@gmail.com>
2024-01-31 16:53:49 +0200

    Posting here

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A script that uses some things to do things"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    theUrl = sys.argv[1]
+    theEmail = sys.argv[2]
+    r = requests.post(theUrl, data={"email": theEmail})
+    print(r.content.decode('utf-8'))
