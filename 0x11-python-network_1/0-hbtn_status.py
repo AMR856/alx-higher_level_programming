8b176c426772a61baa545c26cd2f8378ec4d47ce
ameralnas <amer.live477@gmail.com>
2024-01-31 15:37:12 +0200

    Hi dude

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """A script that uses some things to do things"""
 import urllib.request
-import sys
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         html = response.read()
         print("Body response:")
         print(f"\t- type: {type(html)}")
