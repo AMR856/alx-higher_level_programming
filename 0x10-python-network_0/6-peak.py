d2c07581d6cb881b6d602b83c1100006cf3dc3ba
ameralnas <amer.live477@gmail.com>
2024-01-31 03:02:01 +0200

    go plz

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""A file to find the peak"""
+
+
 def find_peak(list_of_integers):
+    """Here is the function btw
+
+    list_of_integers: The list btw
+    """
     tempPeak = -2147483648
     for myNum in list_of_integers:
         if myNum > tempPeak:
