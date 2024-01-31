a2b9646417e5dbeebd8ec4d490724b79a6156df8
ameralnas <amer.live477@gmail.com>
2024-01-31 04:08:24 +0200

    Hi again

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,7 @@
 """A script that uses some things to do things"""
 import urllib.request
 import sys
-with urllib.request.urlopen(sys.argv[1]) as response:
-    theHeader = response.getheader("X-Request-Id")
-    print(theHeader)
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        theHeader = response.getheader("X-Request-Id")
+        print(theHeader)
