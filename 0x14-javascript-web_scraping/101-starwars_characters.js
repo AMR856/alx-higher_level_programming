ac5c8cc6d158e2dfe1bf9e812c48d825c983c6ee
ameralnas <amer.live477@gmail.com>
2024-02-20 18:53:45 +0200

    That's working

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,23 @@
 #!/usr/bin/node
 const process = require('process');
-const request = require('request');
+const request = require('sync-request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 const fullUrl = url.concat('', process.argv[2]);
-request(fullUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-  const jsonBody = JSON.parse(body);
+const response = request('GET', fullUrl);
+
+if (response.statusCode === 200) {
+  const jsonBody = JSON.parse(response.getBody('utf8'));
+
   for (let i = 0; i < jsonBody.characters.length; i++) {
-    request(jsonBody.characters[i], (err, response1, body1) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(JSON.parse(body1).name);
-    });
+    const characterResponse = request('GET', jsonBody.characters[i]);
+    if (characterResponse.statusCode === 200) {
+      const characterName = (JSON.parse(characterResponse.getBody('utf8'))).name;
+      console.log(characterName);
+    } else {
+      console.log(`Error fetching character: ${characterResponse.statusCode}`);
+    }
   }
-});
+} else {
+  console.log(`Error fetching film details: ${response.statusCode}`);
+}
