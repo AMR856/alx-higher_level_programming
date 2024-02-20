5d5c3ee736450677150e001d48ce5958ec4ff08f
ameralnas <amer.live477@gmail.com>
2024-02-20 17:58:18 +0200

    Try this

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,10 +8,15 @@ request(url, (error, response, body) => {
     console.log(error);
   }
   const jsonBody = JSON.parse(body);
-  const myObject = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0 };
+  const myObject = {};
   for (let i = 0; i < jsonBody.length; i++) {
     if (jsonBody[i].completed === true) {
-      myObject[jsonBody[i].userId] = myObject[jsonBody[i].userId] + 1;
+      if (myObject[jsonBody[i].userId] === undefined) {
+        myObject[jsonBody[i].userId] = 1;
+      }
+      else {
+        myObject[jsonBody[i].userId] = myObject[jsonBody[i].userId] + 1;
+      }
     }
   }
   console.log(myObject);
