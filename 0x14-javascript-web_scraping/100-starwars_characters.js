bcfec2e183cb88760193d3b9f19950b1d52b67fe
ameralnas <amer.live477@gmail.com>
2024-02-20 18:40:55 +0200

    Lol

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const fullUrl = url.concat('', process.argv[2]);
+request(fullUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const jsonBody = JSON.parse(body);
+  for (let i = 0; i < jsonBody.characters.length; i++) {
+    request(jsonBody.characters[i], (err, response1, body1) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(JSON.parse(body1).name);
+    });
+  }
+});
