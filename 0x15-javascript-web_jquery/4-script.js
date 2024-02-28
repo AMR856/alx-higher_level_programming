2bdde0c3e25a4efec1d6b84afa5afc752e94bb48
ameralnas <amer.live477@gmail.com>
2024-02-28 22:14:02 +0200

    Added some files
    second task
    third task
    fourth task

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+$("DIV#toggle_header").click( function() { 
+    if ($("header").hasClass("red"))
+    {
+        $("header").removeClass("red");
+        $("header").addClass("green");
+    }
+    else
+    {
+        $("header").removeClass("green");
+        $("header").addClass("red");
+    }
+});
